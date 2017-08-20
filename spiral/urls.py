4af99b259f1cf19728d2515e8c41bf0583e0260b
ky <kwinborn@gmail.com>
2017-08-20 13:08:11 -0400

    adding first view

--- spiral/urls.py
+++ spiral/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.people_list, name='people_list'),
+    url(r'^$', views.persons_list, name='persons_list'),
 ]
--- spiral/views.py
+++ spiral/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Person, Address
 
-def people_list(request):
-    return render(request, 'spiral/people_list.html', {})
+def persons_list(request):
+    persons = Person.objects.filter(created_date__lte=timezone.now()).order_by('created_date')
+    return render(request, 'spiral/persons_list.html', {'persons': persons})
