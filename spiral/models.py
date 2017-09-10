e66443ede66dd2231971e6ff27517f25ccf7294b
ky <kwinborn@gmail.com>
2017-09-09 22:55:41 -0400

    extending templates and adding address to person model

--- spiral/models.py
+++ spiral/models.py
@@ -1,23 +1,6 @@
 from django.db import models
 from django.utils import timezone
 
-class Person(models.Model):
-    first_name = models.CharField(max_length=60)
-    last_name = models.CharField(max_length=60)
-    full_name = models.CharField(max_length=60)
-    email = models.EmailField(max_length=60)
-    birthday = models.DateTimeField(blank=True, null=True)
-    created_date = models.DateTimeField(default=timezone.now)
-    activated_date = models.DateTimeField(blank=True, null=True)
-    address1 = models.ForeignKey(Address)
-
-    def activate(self):
-        self.activated_date = timezone.now()
-        self.save()
-
-    def __str__(self):
-        return self.full_name
-
 class Address(models.Model):
     street_name_1 = models.CharField(max_length=100)
     street_name_2 = models.CharField(max_length=100)
@@ -28,3 +11,20 @@ class Address(models.Model):
 
     def __str__(self):
         return self.text
+
+class Person(models.Model):
+    first_name = models.CharField(max_length=60)
+    last_name = models.CharField(max_length=60)
+    full_name = models.CharField(max_length=60)
+    email = models.EmailField(max_length=60)
+    birthday = models.DateTimeField(blank=True, null=True)
+    created_date = models.DateTimeField(default=timezone.now)
+    activated_date = models.DateTimeField(blank=True, null=True)
+    address1 = models.ForeignKey('Address')
+
+    def activate(self):
+        self.activated_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.full_name
